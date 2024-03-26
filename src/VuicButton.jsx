170004bc7589861c675d8b093ca85248522ea9f3
Mahmoud Zalt <mahmoud@zalt.me>
2024-03-26 19:50:12 +0100

    get back animation

--- src/VuicButton.jsx
+++ src/VuicButton.jsx
@@ -5,11 +5,13 @@ import { FaMicrophone } from 'react-icons/fa';
 const VuicButton = ({ buttonText = 'Record', ...props }) => {
     const vuic = useVuic();
     const [recordingState, setRecordingState] = useState('idle'); // 'idle', 'recording', 'processing'
+    const [isButtonDisabled, setButtonDisabled] = useState(false); // New state variable
 
     useEffect(() => {
         if (vuic) {
             const handleStateChange = (newState) => {
                 setRecordingState(newState);
+                setButtonDisabled(newState !== 'idle'); // Disable button if not idle
             };
 
             vuic.on('stateChange', handleStateChange);
@@ -23,19 +25,21 @@ const VuicButton = ({ buttonText = 'Record', ...props }) => {
     const handleButtonClick = () => {
         if (vuic) {
             vuic.startVoiceRecording();
+            setButtonDisabled(true); // Disable button after click
         }
     };
 
     // Define a mapping from state to button background color
     const stateToColor = {
         idle: '#ff0000',
-        recording: '#00ff00',
-        processing: '#0000ff',
+        recording: '#008000',
+        processing: '#007cff',
     };
 
     return (
         <button
             onClick={handleButtonClick}
+            disabled={isButtonDisabled} // Disable button based on state
             style={{
                 position: 'fixed',
                 bottom: '20px',
@@ -52,11 +56,19 @@ const VuicButton = ({ buttonText = 'Record', ...props }) => {
                 border: 'none',
                 boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.7)',
             }}
+            onMouseOver={(e) => {
+                e.currentTarget.children[0].style.transform =
+                    'scale(1.5) rotate(360deg)'; // Scale up and rotate icon on hover
+            }}
+            onMouseOut={(e) =>
+                (e.currentTarget.children[0].style.transform =
+                    'scale(1) rotate(0deg)')
+            } // Scale down and reset rotation when hover ends
             {...props}
         >
             <FaMicrophone
                 style={{
-                    transition: 'transform 0.3s ease-in-out',
+                    transition: 'transform 0.3s ease-in-out', // Add transition for smooth scaling
                 }}
             />
         </button>
