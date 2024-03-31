efc9acdeecbe64f6361c4dc89c25f4143596e164
Mahmoud Zalt <mahmoud@zalt.me>
2024-03-31 17:07:03 +0200

    G

--- src/core/AudioPlayer.js
+++ src/core/AudioPlayer.js
@@ -3,90 +3,90 @@ import config from './config';
 import EventEmitter from './EventEmitter';
 
 class AudioPlayer {
-
     constructor() {
         this.eventEmitter = new EventEmitter();
-        this.audioContext = null; // Add an audio context property
+        this._setupAudioContext();
+        this._loadSounds();
+    }
 
+    playRecordingTone(audioObj, volume = 0.20) {
+        console.log('--[VUIC]-- playRecordingTone');
+        this._resumeAudioContextIfSuspended();
+        this._playAudioObject(audioObj, volume);
+    }
+
+    playAiReply = (audioFileUrl) => {
+        console.log('--[VUIC]-- playAiReply');
+        this._checkAudioSupportAndPlayReply(audioFileUrl);
+    };
+
+    _setupAudioContext() {
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        if (window.AudioContext) {
+            this.audioContext = new AudioContext();
+            this._unlockAudioContextOnFirstInteraction();
+        }
+    }
+
+    _unlockAudioContextOnFirstInteraction() {
+        document.documentElement.addEventListener('click', () => {
+            if (this.audioContext.state === 'suspended') {
+                this.audioContext.resume();
+            }
+        }, { once: true });
+    }
+
+    _loadSounds() {
         try {
-            // Preload the start and end sounds
             this.startSound = new Audio(config.startSoundFileUrl);
             this.endSound = new Audio(config.endSoundFileUrl);
-            this.setupAudioContext(); // Setup audio context on initialization
         } catch (error) {
             console.error('Failed to load audio files:', error);
         }
     }
 
-    setupAudioContext() {
-        // Use AudioContext to manage playback
-        window.AudioContext = window.AudioContext || window.webkitAudioContext;
-        if (window.AudioContext) {
-            this.audioContext = new AudioContext();
-            // Unlock the audio context on the first user interaction
-            document.documentElement.addEventListener('click', () => {
-                if (this.audioContext.state === 'suspended') {
-                    this.audioContext.resume();
-                }
-            }, { once: true });
+    _resumeAudioContextIfSuspended() {
+        if (this.audioContext && this.audioContext.state === 'suspended') {
+            this.audioContext.resume();
         }
     }
 
-    playRecordingTone(audioObj, volume = 0.20) {
-        console.log('--[VUIC]-- playRecordingTone');
-        if (this.audioContext && this.audioContext.state === 'suspended') {
-            this.audioContext.resume(); // Ensure the audio context is resumed
-        }
-
+    _playAudioObject(audioObj, volume) {
         try {
             audioObj.volume = volume;
             audioObj.play();
         } catch (error) {
             console.error('Failed to play sound:', error);
         }
-
-        return audioObj;
     }
 
-    playAiReply = (audioFileUrl) => {
-        console.log('--[VUIC]-- playAiReply');
-        // Check if the browser supports the Audio API
+    _checkAudioSupportAndPlayReply(audioFileUrl) {
         if (!window.Audio || !this.audioContext) {
             this.eventEmitter.emitStateChange(EventEmitter.STATE_IDLE);
             console.error('This browser does not support the Audio API');
             return;
         }
 
-        let audio;
-        try {
-            // Instead of creating a new Audio, connect to the existing AudioContext
-            const source = this.audioContext.createBufferSource();
-            fetch(audioFileUrl)
-                .then(response => response.arrayBuffer())
-                .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
-                .then(audioBuffer => {
-                    source.buffer = audioBuffer;
-                    source.connect(this.audioContext.destination);
-                    source.start(0);
-                    this._handleAudioEvents(source); // Adjusted to handle events on source
-                });
-        } catch (error) {
-            this.eventEmitter.emitStateChange(EventEmitter.STATE_IDLE);
-            console.error('Failed to load and play audio file:', error);
-            return;
-        }
-    };
-
-    _handleAudioEvents = (audio) => {
-        // Adjust event handling for AudioBufferSourceNode
-        audio.onended = () => {
-            this.eventEmitter.emitStateChange(EventEmitter.STATE_IDLE);
-        };
-
-        // Note: For AudioBufferSourceNode, 'onplay' and 'onerror' events are not directly available like they are for the HTMLAudioElement.
-        // You may need to adjust your event handling logic here based on your application's needs.
+        this._loadAudio(audioFileUrl)
+            .then(this.playAudio)
+            .catch(error => {
+                this.eventEmitter.emitStateChange(EventEmitter.STATE_IDLE);
+                console.error('Failed to load and play audio file:', error);
+            });
     }
 
+    _loadAudio = (audioFileUrl) => {
+        return fetch(audioFileUrl)
+            .then(response => response.arrayBuffer())
+            .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer));
+    };
+
+    playAudio = (audioBuffer) => {
+        const source = this.audioContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(this.audioContext.destination);
+        source.start(0);
+    };
 }
 
 export default AudioPlayer;
